5d05e070e56ea3b8061132ae10fb9d9dc9a81916
Bhupendra Gautam <bhupendragtm777@gmail.com>
2022-11-22 12:37:36 +0000

    Post, Patch and Delete Completed

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 
 app.use(express.json());
 
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ entended: false }));
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ entended: false }));
 
 app.get("/", (req, res) => {
   res.send(`<h1>Hello ${user}, Now You Can Create API Using Express1.<h1>`);
